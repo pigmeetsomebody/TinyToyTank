93bfde2dee762c36d9964bc5f2d5c2c0334560d4
jianjianzhu <jianjianzhu@tencent.com>
2021-11-06 15:27:54 +0800

    Initial Commit

---
+++ TinyToyTank/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TinyToyTank
+//
+//  Created by 朱彦谕 on 2021/11/6.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
